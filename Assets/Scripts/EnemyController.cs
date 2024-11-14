24a74b1ed08eaa230a62d2e6ee9a69045421f152
mjpurin <mjpurin@gmail.com>
2024-11-14 12:45:44 +0900

    Part4

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -11,11 +12,14 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rb;
     float timer;
     int direction = 1;
+
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,9 +34,13 @@ public class EnemyController : MonoBehaviour
 
         if(isVertical){
             pos.y = pos.y + Time.deltaTime * speed * direction;
+            anim.SetFloat("MoveX",0);
+            anim.SetFloat("MoveY",direction);
 
         }else{
             pos.x = pos.x + Time.deltaTime * speed * direction;
+            anim.SetFloat("MoveX",direction);
+            anim.SetFloat("MoveY",0);
 
         }
         rb.MovePosition(pos);
